fdddeff18902de50694d689af8269481fe32c081
Kjell <kjell@leanside.com>
2013-10-24 10:29:46 -0500

    Stop angular-segmentio logspam, don't track dev

--- js/vendor/angular-segmentio.js
+++ js/vendor/angular-segmentio.js
@@ -10,13 +10,10 @@ angular.module('segmentio', ['ng'])
         var methodFactory = function (type) {
             return function () {
                 var args = Array.prototype.slice.call(arguments, 0);
-                $log.info('Call segmentio API with', type, args);
                 if(analytics.initialized) {
-                    $log.info('Segmentio API initialized, calling API');
                     $window.analytics[type].apply($window.analytics, args);
                 }
                 else {
-                    $log.info('Segmentio API not yet initialized, queueing call');
                     $window.analytics.push([type].concat(args));
                 }
             };
