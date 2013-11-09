eef6e78a02090e5e43ff246e19e4445e230427b7
Kjell <kjell@leanside.com>
2013-11-09 01:36:50 -0600

    ng-poster directive

--- js/ng-presenter.js
+++ js/ng-presenter.js
@@ -640,4 +640,16 @@
       $scope.$apply()
     }
   }])
+
+  app.directive('ngPoster', function() {
+    return {
+      priority: 99, // it needs to run after the attributes are interpolated
+      link: function(scope, element, attr) {
+        attr.$observe('ngPoster', function(value) {
+          if (!value) return;
+          attr.$set('poster', value);
+        })
+      }
+    }
+  }) // https://github.com/angular/angular.js/blob/v1.2.0/src/ng/directive/booleanAttrs.js#L86
 })()
