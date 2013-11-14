08fee1a6f6d8fbd3ab3d38baf6d2a393d553f720
Kjell <kjell@leanside.com>
2013-11-14 12:13:48 -0600

    Only beckmann

--- js/ng-presenter.js
+++ js/ng-presenter.js
@@ -7,20 +7,13 @@
   app.config(
     ['$routeProvider', function($routeProvider) {
       return $routeProvider.when('/', {
-        templateUrl: 'views/index.html',
-        controller: 'mainCtrl'
+        redirectTo: '/o/1270'
       }).when('/o/:id', {
         templateUrl: 'views/object.html',
         controller: 'ObjectCtrl'
-      }).when('/a/:id', {
-        templateUrl: 'views/annotations.html',
-        controller: 'notesCtrl'
       }).when('/stories/:id', {
         templateUrl: 'views/story.html',
         controller: 'storyCtrl'
-      }).when('/goldweights', {
-        templateUrl: 'views/goldweights.html',
-        controller: 'goldweightsCtrl'
       }).otherwise({
         redirectTo: '/'
       })
