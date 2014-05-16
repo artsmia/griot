f30c02e455c7c70296a89759e01afe18eb70f2b7
Tom Borger <thomas.borger@gmail.com>
2014-05-16 09:08:02 -0500

    Non-JS implementation of vcenter directive

--- js/bundle.js
+++ js/bundle.js
@@ -175,7 +175,7 @@ app.controller('mainCtrl', ['$scope', '$routeParams', 'notes', 'segmentio', '$ro
 
       };
 
-     typeof $rootScope.loaded == 'undefined' ? $timeout( initPackery, 750 ) : $timeout( initPackery, 0 );
+      typeof $rootScope.loaded == 'undefined' ? $timeout( initPackery, 750 ) : $timeout( initPackery, 0 );
 
       /*
       var initPackery = function() {
@@ -737,37 +737,12 @@ app.directive("scroll", function ($window) {
 
 
 },{}],12:[function(require,module,exports){
-app.directive('vcenter', function() {
+app.directive( 'vcenter', function(){
 
 	return{
-		restrict:'C',
-		replace:false,
-		controller: function( $scope, $element, $attrs ) {
-
-			var _scope = $scope;
-
-			$scope.vc_height = 0;
-			$scope.vc_parent = 0;
-
-			$scope.vc_interval = setInterval( function(){
-
-				if( jQuery( $element ).height() !== $scope.vc_height || jQuery( $element ).parent().height() !== $scope.vc_parent ) {
-		  		var topBuffer = ( jQuery( $element ).parent().height() - jQuery( $element ).height() ) / 2;
-		  		jQuery( $element ).css({
-		  			'position':'relative', 
-		  			'top':topBuffer 
-		  		});
-		  		$scope.vc_height = jQuery( $element ).height();
-		  		$scope.vc_parent = jQuery( $element ).parent().height();
-		  	}
-			}, 100 );
-
-			$scope.$on("$destroy", function(){
-        clearInterval(_scope.vc_interval);
-				console.log( 'clearing' );
-			});
-
-		}
+		restrict: 'C',
+		transclude: true,
+		template: "<div class='vcenter_outer'><div class='vcenter_inner' ng-transclude></div></div>"
 	}
 
 });
