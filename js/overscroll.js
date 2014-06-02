18a891649f298c6898eaf39a881c7e01faf0d9af
Tom Borger <thomas.borger@gmail.com>
2014-06-02 15:20:24 -0500

    Tablet fixes, including horizontal overscroll extension

--- js/overscroll.js
+++ js/overscroll.js
@@ -7,19 +7,30 @@ $(document).on('touchmove',function(e){
 });
 
 $('body').on('touchstart', selScrollable, function(e) {
+
   if (e.currentTarget.scrollTop === 0) {
     e.currentTarget.scrollTop = 1;
   } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
     e.currentTarget.scrollTop -= 1;
   }
+
+  if( e.currentTarget.scrollLeft === 0) {
+    e.currentTarget.scrollLeft = 1;
+  } else if ( e.currentTarget.scrollWidth === e.currentTarget.scrollLeft + e.currentTarget.offsetWidth ) {
+    e.currentTarget.scrollLeft -= 1;
+  }
 });
 
-$('body').on('touchmove', selScrollable, function(e) {
-    // Only block default if internal div contents are large enough to scroll
-    // Warning: scrollHeight support is not universal. (http://stackoverflow.com/a/15033226/40352)
-    if($(this)[0].scrollHeight > $(this).innerHeight()) {
-        e.stopPropagation();
-    }
+$( 'body' ).on( 'touchmove', selScrollable, function( e ){
+  // Only block default if internal div contents are large enough to scroll
+  // Warning: scrollHeight support is not universal. (http://stackoverflow.com/a/15033226/40352)
+  if( $(this)[0].scrollHeight > $(this).innerHeight() ){
+    e.stopPropagation();
+  }
+
+  if( $(this)[0].scrollWidth > $(this).innerWidth() ){
+    e.stopPropagation();
+  }
 });
 
 $(window).on('orientationchange', function() {
